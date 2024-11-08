da84575752e5e3b5a43b350a802eaf73e4f9f025
Susan Chisi <bsc-com-12-21@unima.ac.mw>
2024-11-08 06:27:22 +0200

    adding  sellers button

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LogInPage = ()=> {
     return (
 
-        <div className='flex justify-center items-center h-screen'>
+        <div className='flex justify-center items-center'>
             <div className = "bg-white grid md:grid-cols-4 gap-7 items-center">
                     
                 <div className='bg-orange-500 p-20 w-96 max-h justify-normal py-56  '>
@@ -11,7 +11,7 @@ const LogInPage = ()=> {
                 </div>
 
                 <div className= 'bg-white p-20 w-96 max-h justify-normal py-56'>
-                    <h2 className='font-bold text-center-9pxl mb-6'>Create an account</h2>
+                    <h1 className='font-bold text-center-9pxl mb-6'>Create an account</h1>
                     <p className='text-gray-500 mb-6'>Enter your details below</p>
                     <form className='space-y-8'>
                         <div>
@@ -20,7 +20,9 @@ const LogInPage = ()=> {
                         <div>
                             <input type='password' placeholder='Password' className='w-full border rounded-md px-7 py-3'></input>
                         </div>
-                        <button type='submit' className='hover:bg-red-600 transition-colors w-full bg-red-500 text-white rounded-md py-4 px-10 justify-center'>Create account</button>
+                        
+                        <button type='submit' className='hover:bg-red-900 transition-colors w-full bg-red-500 text-white rounded-md py-4 px-4 '>Seller</button>
+                        <button type='submit' className='hover:bg-red-900 transition-colors w-full bg-red-500 text-white rounded-md py-4 px-10 justify-center'>Create account</button>
                         <div className='text center mt-5'>
                             <span className='text-gray-500'>Already have an account?</span>{''}
                             <a href='#' className='text-blue-800'>Log in</a>
