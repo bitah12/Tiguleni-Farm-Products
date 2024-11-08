04cc9e4ee6533b8ba9a8aed8570a30b787dbb86d
Susan Chisi <bsc-com-12-21@unima.ac.mw>
2024-11-08 06:10:57 +0200

    moving the page to the center of the page  vertically

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const LogInPage = ()=> {
     return (
 
-        <div className='flex items-center'>
+        <div className='flex justify-center items-center h-screen'>
             <div className = "bg-white grid md:grid-cols-4 gap-7 items-center">
                     
-                    <div className='bg-orange-500 p-20 w-96 max-h justify-normal py-56  '>
-                        <img src="https://img.freepik.com/premium-vector/smartphone-with-purchase-icon-screen-place-red-shopping-cart-with-unfolded-receipt-paper-draped-edge-shopping-cart-online-shopping-conceptvector-3d-isolated-orange-backgroud_425581-1.jpg?w=740" alt="Shopping basket" className="h-auto w-full" />
-                    </div>
+                <div className='bg-orange-500 p-20 w-96 max-h justify-normal py-56  '>
+                    <img src="https://img.freepik.com/premium-vector/smartphone-with-purchase-icon-screen-place-red-shopping-cart-with-unfolded-receipt-paper-draped-edge-shopping-cart-online-shopping-conceptvector-3d-isolated-orange-backgroud_425581-1.jpg?w=740" alt="Shopping basket" className="h-auto w-full" />
+                </div>
 
-                    <div className= 'bg-white p-20 w-96 max-h justify-normal py-56'>
+                <div className= 'bg-white p-20 w-96 max-h justify-normal py-56'>
                     <h2 className='font-bold text-center-9pxl mb-6'>Create an account</h2>
                     <p className='text-gray-500 mb-6'>Enter your details below</p>
                     <form className='space-y-8'>
@@ -26,8 +26,7 @@ const LogInPage = ()=> {
                             <a href='#' className='text-blue-800'>Log in</a>
                         </div>
                     </form>
-                    </div>
-               
+                </div>
             </div>
 
         </div>
