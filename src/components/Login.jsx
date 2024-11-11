95e62855d62869aea0f59f67069bc883423127d9
Susan Chisi <bsc-com-12-21@unima.ac.mw>
2024-11-11 15:26:54 +0200

    creating a form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,7 +8,34 @@ const LoginPage = () => {
      
         <div className="bg-white p-8 rounded-r-lg shadow-lg w-full max-w-md">
             <h1 className="text-2xl font-bold mb-4">Log In to Tiguleni</h1>
-            
+            <form>
+                <div className="mb-5">
+                <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
+                    Email or Phone Number
+                </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              className="rounded w-full py-3 px-4 text-gray-700"
+              placeholder="Enter your email or phone number"
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <label htmlFor="password" className="block text-gray-700 font-bold mb-2">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className=" border rounded w-full py-3 px-4 text-gray-700 "
+              placeholder="Enter your password"
+              required/>
+          </div>
+          
+        </form>
         
       </div>
       <div className="absolute top-0 right-0 mt-4 mr-4">
