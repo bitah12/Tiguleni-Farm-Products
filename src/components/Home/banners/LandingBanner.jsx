6a16e0e36a82ee39e1d1f65f794b667a9f0a5fcc
Rbitah <bsc-35-21@unima.ac.mw>
2024-11-12 15:37:08 +0200

    landing banner

---
+++ src/components/Home/banners/LandingBanner.jsx
@@ -0,0 +1,15 @@
+import LandingComplete from '/src/assets/LandingComplete.jpg'
+
+function LandingBanner() {
+    return (
+      <div className='mt-10  flex justify-center'>
+        <img 
+          src={LandingComplete} 
+          alt="Landing Banner" 
+          className=" hidden lg:block w-full h-auto max-w-[1138px] object-cover"
+        />
+      </div>
+    );
+  }
+  
+  export default LandingBanner;
