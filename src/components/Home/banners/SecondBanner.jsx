4a1dd4d77e60da5336c88ffd5ba3baf4bc90fddf
Rbitah <bsc-35-21@unima.ac.mw>
2024-11-12 15:37:17 +0200

    second banner

---
+++ src/components/Home/banners/SecondBanner.jsx
@@ -0,0 +1,16 @@
+import SecondBannerComplete from "/src/assets/SecondBannerComplete.png";
+
+
+function SecondBanner() {
+    return (
+      <div className='mt-32  flex justify-center'>
+        <img 
+          src={SecondBannerComplete} 
+          alt="Landing Banner" 
+          className=" hidden lg:block w-full h-auto max-w-[1138px] object-cover"
+        />
+      </div>
+    );
+  }
+  
+  export default SecondBanner;
