9d35d208f73a50259704905a126d11c1fb147910
Rbitah <bsc-35-21@unima.ac.mw>
2024-11-12 19:32:57 +0200

    added new photos

--- src/components/Home/products/Products.jsx
+++ src/components/Home/products/Products.jsx
@@ -1,16 +1,16 @@
 const products = [
-    { id: 1, name: 'Product 1', price: 'Mkw10000', rating: 2.5, reviews: 15, image: '/src/assets/ProductpIs.png' },
+    { id: 1, name: 'Product 1', price: 'Mkw10000', rating: 2.5, reviews: 15, image: '/src/components/pictures/banana.jpeg' },
     { id: 2, name: 'Product 2', price: 'Mkw20000', rating: 3.0, reviews: 20, image: '/src/assets/ProductpIs.png' },
     { id: 3, name: 'Product 3', price: 'Mkw30000', rating: 3.5, reviews: 25, image: '/src/assets/ProductpIs.png' },
     { id: 4, name: 'Product 4', price: 'Mkw40000', rating: 4.0, reviews: 30, image: '/src/assets/ProductpIs.png' },
-    { id: 5, name: 'Product 5', price: 'Mkw50000', rating: 4.2, reviews: 35, image: '/src/assets/ProductpIs.png' },
+    { id: 5, name: 'Product 5', price: 'Mkw50000', rating: 4.2, reviews: 35, image: '/src/components/pictures/kilombero.jpg' },
     { id: 6, name: 'Product 6', price: 'Mkw60000', rating: 4.5, reviews: 40, image: '/src/assets/ProductpIs.png' },
     { id: 7, name: 'Product 7', price: 'Mkw70000', rating: 4.7, reviews: 45, image: '/src/assets/ProductpIs.png' },
-    { id: 8, name: 'Product 8', price: 'Mkw80000', rating: 4.8, reviews: 50, image: '/src/assets/ProductpIs.png' },
+    { id: 8, name: 'Product 8', price: 'Mkw80000', rating: 4.8, reviews: 50, image: '/src/components/pictures/kilombero.jpg' },
     { id: 9, name: 'Product 9', price: 'Mkw90000', rating: 4.9, reviews: 55, image: '/src/assets/ProductpIs.png' },
-    { id: 10, name: 'Product 10', price: 'Mkw100000', rating: 5.0, reviews: 60, image: '/src/assets/ProductpIs.png' },
+    { id: 10, name: 'Product 10', price: 'Mkw100000', rating: 5.0, reviews: 60, image: '/src/components/pictures/milk.jpeg' },
     { id: 11, name: 'Product 10', price: 'Mkw100000', rating: 5.0, reviews: 60, image: '/src/assets/ProductpIs.png' },
-    { id: 12, name: 'Product 10', price: 'Mkw100000', rating: 5.0, reviews: 60, image: '/src/assets/ProductpIs.png' },
+    { id: 12, name: 'Product 10', price: 'Mkw100000', rating: 5.0, reviews: 60, image: '/src/components/pictures/urea.jpeg' },
   ];
   
   export default products;
