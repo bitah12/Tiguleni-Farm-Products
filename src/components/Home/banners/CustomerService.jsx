5060059de9aad5c2a0f5d10af052f900d5bbcb75
Rbitah <bsc-35-21@unima.ac.mw>
2024-11-12 15:36:59 +0200

    created customer service componnt

---
+++ src/components/Home/banners/CustomerService.jsx
@@ -0,0 +1,15 @@
+import CustomerServicePic from '/src/assets/CustomerServicePic.png'
+
+function CustomerService() {
+    return (
+      <div className='mt-36  flex justify-center'>
+        <img 
+          src={CustomerServicePic} 
+          alt="Landing Banner" 
+          className=" hidden lg:block w-[600px] h-auto max-w-[1138px] object-cover"
+        />
+      </div>
+    );
+  }
+  
+  export default CustomerService;
