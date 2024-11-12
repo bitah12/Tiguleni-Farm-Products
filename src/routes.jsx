a85c9a9ceff6dd0630d92b73c618195ca1a8b1cc
Rbitah <bsc-35-21@unima.ac.mw>
2024-11-12 15:32:38 +0200

    added new routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,13 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import SellerLayout from "./components/sellerComponents/SellerLayout";
 import AdminLayout from "./components/adminComponents/AdminLayout";
+import ProductBuyNowPage from "./components/buyNow/ProductBuyNowPage";
+import ContactUs from "./components/ContactUs";
+import Home from "./components/Home/Home";
 
 
 export const router = createBrowserRouter([
-    {
+    {path:'/',element:<Home/>},{
         path: "/seller/*",
         element: <SellerLayout></SellerLayout>},
         {
             path: "/admin/*",
-            element: <AdminLayout></AdminLayout>}
+            element: <AdminLayout></AdminLayout>},
+            {
+                path: "/productDetails:productId",
+                element: <ProductBuyNowPage></ProductBuyNowPage>},
+                {path:'/contactus',
+                    element: <ContactUs/>
+                }
 ])
