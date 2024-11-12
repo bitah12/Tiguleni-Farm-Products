82022765eb78cdc160c70f0f6fbd2b455af00abf
Rbitah <bsc-35-21@unima.ac.mw>
2024-11-12 15:39:06 +0200

    third banner

---
+++ src/components/Home/banners/ThirdBanner.jsx
@@ -0,0 +1,15 @@
+import ThirdBannerPic from '/src/assets/ThirdBanner.jpg'
+
+function ThirdBanner() {
+    return (
+      <div className='mt-32  flex justify-center'>
+        <img 
+          src={ThirdBannerPic} 
+          alt="Landing Banner" 
+          className=" hidden lg:block w-full h-auto max-w-[1138px] object-cover"
+        />
+      </div>
+    );
+  }
+  
+  export default ThirdBanner;
