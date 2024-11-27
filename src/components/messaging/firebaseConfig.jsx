56e71f0eb2c5b3c4886ff8a45473dea69a0206d6
Rbitah <bsc-35-21@unima.ac.mw>
2024-11-27 06:46:22 +0200

    fix .env imports

--- src/components/messaging/firebaseConfig.jsx
+++ src/components/messaging/firebaseConfig.jsx
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey:`${import.meta.env.VITE_API_KEY}`,
-authDomain:`${import.meta.env.VITE_URL_FIREBASE}`,
-projectId: `${import.meta.env.VITE_PROJECT_ID}`,
-    storageBucket: `${import.meta.env.VITE_storageBucket}`,
-    messagingSenderId: `${import.meta.env.VITE_messagingSenderId}`,
-    appId: `${import.meta.env.VITE_appId}`,
-    measurementId: `${import.meta.env.VITE_measurementId}`,
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_URL_FIREBASE,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET, 
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID, 
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
